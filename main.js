87bd264c59b9a44e42dac426128a2ebf81157938
tranhoangc5 <tranhoang750@gmail.com>
2019-11-03 21:33:20 +0700

    xong phan header va html phan about me

--- main.js
+++ main.js
@@ -7,3 +7,38 @@ hum.addEventListener("click",function(){
     this.classList.toggle('humberger-clicked')
     dropdown.classList.toggle('dropdown-clicked')
 });
+
+
+/////////background change/////////
+
+const topBackground = Array.from(document.querySelectorAll(".header-img-wraper img"));
+
+function changeBackground(background){
+    let i = 0;
+    //set all the image transition
+    for (let j = 0; j < background.length; j++){
+        background[j].style.cssText = "transition: transform 7s ease-in-out, opacity 0.5s";
+    }
+    background[0].style.opacity = "1";
+    // set time for img
+    setInterval(function(){
+        if (i == background.length -1 ){
+            background[i].style.opacity = "0";
+            background[i].style.transform = "scale(1.2)";
+            i = 0;
+            background[i].style.opacity = "1";
+            background[i].style.transform = "scale(1)";
+        }
+        else{
+            background[i].style.transform = "scale(1.2)";
+            background[i].style.opacity = "0";
+            background[i+1].style.opacity = "1";
+            background[i+1].style.transform = "scale(1)";
+            i++;
+        }
+        console.log('done');
+    },7000)
+}
+changeBackground(topBackground)
+
+////////////////
