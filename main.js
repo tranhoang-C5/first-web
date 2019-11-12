5af2e8a1fdcc394639147fa855bcfe6a7f61246b
tranhoang-c5 <tranhoang750@gmail.com>
2019-11-12 20:17:53 +0700

    sua lai file main

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const hum = document.querySelector(".humberger");
 const dropdown = document.querySelector(".dropdown");
 
 
-hum.addEventListener("click",function(){
+hum.addEventListener("click", function () {
     this.classList.toggle('humberger-clicked')
     dropdown.classList.toggle('dropdown-clicked')
 });
@@ -13,69 +13,65 @@ hum.addEventListener("click",function(){
 
 const topBackground = Array.from(document.querySelectorAll(".header-img-wraper img"));
 
-function changeBackground(background){
+function changeBackground(background) {
     let i = 0;
     //set all the image transition
-    for (let j = 0; j < background.length; j++){
+    for (let j = 0; j < background.length; j++) {
         background[j].style.cssText = "transition: transform 7s ease-in-out, opacity 0.5s";
     }
     background[0].style.opacity = "1";
     // set time for img
-    setInterval(function(){
-        if (i == background.length -1 ){
+    setInterval(function () {
+        if (i == background.length - 1) {
             background[i].style.opacity = "0";
             background[i].style.transform = "scale(1.2)";
             i = 0;
             background[i].style.opacity = "1";
             background[i].style.transform = "scale(1)";
-        }
-        else{
+        } else {
             background[i].style.transform = "scale(1.2)";
             background[i].style.opacity = "0";
-            background[i+1].style.opacity = "1";
-            background[i+1].style.transform = "scale(1)";
+            background[i + 1].style.opacity = "1";
+            background[i + 1].style.transform = "scale(1)";
             i++;
         }
-    },7000)
+    }, 7000)
 }
-changeBackground(topBackground)
+changeBackground(topBackground);
 
 ////////////////SKILL RANGE//////////////////
 
-const abilities = [85,85,70,60];
+const abilities = [85, 85, 70, 60];
 const indexOfAbility = Array.from(document.querySelectorAll(".index-of-ability"))
 const abilityRange = Array.from(document.querySelectorAll(".ability-range-counter"))
 const skillContainer = document.querySelector(".skill-section");
 
 // build a function to count and show result of each ability
-function countAbilities(value,abilityIndex){
+function countAbilities(value, abilityIndex) {
     var i = 0;
 };
-for (let i = 0; i < abilities.length; i++){
+for (let i = 0; i < abilities.length; i++) {
     abilityRange[i].style.width = '0';
 }
-var showSkill = function() {
-    for (let i = 0; i < abilities.length; i++){
-        abilityRange[i].style.width = `${abilities[i]}%`;//set range of ability
+var showSkill = function () {
+    for (let i = 0; i < abilities.length; i++) {
+        abilityRange[i].style.width = `${abilities[i]}%`; //set range of ability
         let j = 0;
         var show = setInterval(() => {
-            if (j == abilities[i]+1){
+            if (j == abilities[i] + 1) {
                 clearInterval(show);
-            }
-            else{
-                indexOfAbility[i].textContent = j +'%';
+            } else {
+                indexOfAbility[i].textContent = j + '%';
                 j++;
             }
-        }, 1600/abilities[i]);
-    } 
+        }, 1600 / abilities[i]);
+    }
 }
-showSkill();
 //run function when scroll top the element
-window.addEventListener('scroll',function runSkillBar(){
-    console.log(window.pageYOffset);
-    if (window.pageYOffset > skillContainer.offsetTop - window.outerHeight + 300 ){
+window.addEventListener('scroll', function runSkillBar() {
+    if (window.pageYOffset > skillContainer.offsetTop - window.outerHeight + 300) {
         showSkill();
-        this.removeEventListener('scroll',runSkillBar);
+        this.removeEventListener('scroll', runSkillBar);
     }
 });
 
@@ -84,13 +80,13 @@ window.addEventListener('scroll',function runSkillBar(){
 
 ////////////detail work///////////////
 const works = Array.from(document.querySelectorAll(".work-box"));
-var activeBox = function(){
-    works.forEach(elem => elem.classList.remove('work-box-active'));//restart box to default
+var activeBox = function () {
+    works.forEach(elem => elem.classList.remove('work-box-active')); //restart box to default
     this.classList.add('work-box-active');
 };
 
-for(let i = 0; i< works.length;i++){
-    works[i].addEventListener('click',activeBox)
+for (let i = 0; i < works.length; i++) {
+    works[i].addEventListener('click', activeBox)
 };
 
 
